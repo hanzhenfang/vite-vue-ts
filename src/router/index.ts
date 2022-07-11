4b46e978321f9f226ee077c90d6b90e8b3a8f0a0
hanzhenfang <hanzhenfffang@gmail.com>
2022-07-11 09:06:09 +0800

    add

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,11 +7,6 @@ import mulTable from '@/components/mulTable.vue';
 import Faxian from '@/weiChat/faxian.vue';
 import Pengyouquan from '@/weiChat/pengyouquan.vue';
 import Gerenzhuye from '@/weiChat/gerenzhuye.vue';
-import hanzhenfang from '@/weiChat/gerenzhuye.vue';
-<<<<<<< HEAD
-import { E } from '../utils/vimtext';
-=======
->>>>>>> c95133e87f861fc83129aec88872a57dd49640b9
 
 export  const router = createRouter({
   history: createWebHistory(),
@@ -68,20 +63,8 @@ export  const router = createRouter({
 })
 
 router.beforeEach((to,from,next)=>{
-<<<<<<< HEAD
- console.log('to',to)
-  if(to.fullPath==="/pengyouquan") {
-    if(to.meta.isAuth)
-    next()
-    else{
-      alert('密码错误')
-    }
-  }
-  else next()
-=======
   console.log('to',to)
   console.log('from',from)
   console.log('next',next)
   next()
->>>>>>> c95133e87f861fc83129aec88872a57dd49640b9
 })
--- src/weiChat/pengyouquan.vue
+++ src/weiChat/pengyouquan.vue
@@ -1,28 +1,20 @@
 <script lang="ts" setup>
-import { onMounted } from 'vue';
-import {useRoute} from 'vue-router'
+import { onMounted } from "vue";
+import { useRoute } from "vue-router";
 
-onMounted(()=>{
-console.log('11useRoute()',useRoute())
-<<<<<<< HEAD
-
-=======
->>>>>>> c95133e87f861fc83129aec88872a57dd49640b9
-})
-
+onMounted(() => {
+  console.log("11useRoute()", useRoute());
+});
 </script>
 
 <template>
-<transition name="fade" mode="out-in">
-
-   <div>
-    <router-link :to="{name:'gerenzhuye'}">
-      <img src="../assets/朋友圈.jpg" />
-    </router-link>
-  </div>
-
-</transition>
- 
+  <transition name="fade" mode="out-in">
+    <div>
+      <router-link :to="{ name: 'gerenzhuye' }">
+        <img src="../assets/朋友圈.jpg" />
+      </router-link>
+    </div>
+  </transition>
 </template>
 <style>
 /* .fang-enter-from {
@@ -37,17 +29,19 @@ transform: translateX(-100%);
   transform: translateX(0);
 } */
 .fade-leave-active {
-  transition:  3s ease;
+  transition: 3s ease;
 }
 .fade-enter-active {
   transition: 3s linear;
 }
-.fade-enter-to,.fade-leave-from {
- transform:translateX(0);
-opacity: 1;
- }
-.fade-leave-to,.fade-enter-from {
-  transform:translateX(-100%);
-  opacity:0
+.fade-enter-to,
+.fade-leave-from {
+  transform: translateX(0);
+  opacity: 1;
+}
+.fade-leave-to,
+.fade-enter-from {
+  transform: translateX(-100%);
+  opacity: 0;
 }
 </style>
