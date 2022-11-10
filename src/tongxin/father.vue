6c572efb7902a4d78f862189e9604543f828be99
hanzhenfang <hanzhenfffang@gmail.com>
2022-11-10 15:34:01 +0800

    test-push

--- src/tongxin/father.vue
+++ src/tongxin/father.vue
@@ -6,6 +6,7 @@ import Children from "./children.vue";
 const count = ref<number>(0);
 
 //tips: 也非常简单，父组件在注册这个函数的时候，提前声明一个形参
+//tips: 测试一下 push
 function addCount(childrenMessage: number, author: string, age: Number) {
   count.value = count.value + childrenMessage;
   console.log(author);
